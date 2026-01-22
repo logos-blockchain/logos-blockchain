5d8798b746edf92aeb0105e2ecec1e16e42cf6e9
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-01-22 11:08:31 +0100

    Add missing flags to cargo hack command.

--- scripts/cargo-hack-check/script.py
+++ scripts/cargo-hack-check/script.py
@@ -319,9 +319,10 @@ class CargoHackCheckEntry:
         return hash(self.manifest_path_posix)
 
     def as_feature_powerset_command(self) -> List[str]:
-        # TODO: Current command used: "cargo hack check --feature-powerset --keep-going --all-targets"
+        # Changing this doesn't void the cache.
+        # TODO: Consider adding the command line options to the cache key.
         return [
-            "cargo", "hack", "check", "--feature-powerset", "--no-dev-deps", "--manifest-path", self.manifest_path_posix
+            "cargo", "hack", "check", "--feature-powerset", "--all-targets", "--no-dev-deps", "--manifest-path", self.manifest_path_posix
         ]
 
 
