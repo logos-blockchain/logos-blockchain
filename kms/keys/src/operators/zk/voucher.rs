2dd41ec34a55fdf467f73b472a4f10ea8617b5d9
danielSanchezQ <3danimanimal@gmail.com>
2026-01-27 09:02:08 +0000

    Implement voucher derivation from zk key in kms

---
+++ kms/keys/src/operators/zk/voucher.rs
@@ -0,0 +1,27 @@
+use std::convert::Infallible;
+
+use lb_groth16::Fr;
+use lb_poseidon2::{Digest as _, Poseidon2Bn254Hasher as ZkHasher, ZkHash};
+use tokio::sync::oneshot;
+use tracing::error;
+
+use crate::keys::{ZkKey, secured_key::SecureKeyOperator};
+
+pub struct VoucherOperator {
+    index: Fr,
+    result_channel: oneshot::Sender<ZkHash>,
+}
+
+#[async_trait::async_trait]
+impl SecureKeyOperator for VoucherOperator {
+    type Key = ZkKey;
+    type Error = Infallible;
+
+    async fn execute(self: Box<Self>, key: &Self::Key) -> Result<(), Self::Error> {
+        let voucher = ZkHasher::digest(&[*key.as_fr(), self.index]);
+        if self.result_channel.send(voucher).is_err() {
+            error!("Failed to send voucher via channel");
+        }
+        Ok(())
+    }
+}
