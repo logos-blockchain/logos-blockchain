8efcdc484a39bc9d70c7a634a0108a1bf5f0e6c1
gusto <bacv@users.noreply.github.com>
2026-02-05 10:03:18 +0200

    Update testnet/faucet/src/faucet.rs
    Co-authored-by: Hansie Odendaal <39146854+hansieodendaal@users.noreply.github.com>

--- testnet/faucet/src/faucet.rs
+++ testnet/faucet/src/faucet.rs
@@ -75,7 +75,7 @@ impl Faucet {
             ));
         }
 
-        println!("Dripping 5% of balance: {amount_to_send} units to {recipient_pk:?}",);
+        println!("Dripping {}% of balance: {amount_to_send} units to {recipient_pk:?}", self.drip_rate);
 
         let body = WalletTransferFundsRequestBody {
             tip: None,
