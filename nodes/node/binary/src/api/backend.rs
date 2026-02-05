37606e9fa45675647abac13fd19aa2c74ebf0a97
David Rusu <davidrusu.me@gmail.com>
2026-02-05 11:58:24 +0400

    chore(fmt)

--- nodes/node/binary/src/api/backend.rs
+++ nodes/node/binary/src/api/backend.rs
@@ -20,8 +20,8 @@ use lb_core::{
     header::HeaderId,
     mantle::{SignedMantleTx, Transaction},
 };
-pub use lb_http_api_common::settings::AxumBackendSettings;
 use lb_http_api_common::paths;
+pub use lb_http_api_common::settings::AxumBackendSettings;
 use lb_sdp_service::{mempool::SdpMempoolAdapter, wallet::SdpWalletAdapter};
 use lb_services_utils::wait_until_services_are_ready;
 use lb_storage_service::{StorageService, backends::rocksdb::RocksBackend};
