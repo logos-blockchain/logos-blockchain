c200acaaa9c4961ef6b827c798f5f88304e835f2
gusto <bacvinka@gmail.com>
2026-02-12 21:52:53 +0200

    Directory creation on first run

--- testnet/scripts/setup_cfgsync_server.sh
+++ testnet/scripts/setup_cfgsync_server.sh
@@ -5,6 +5,9 @@ rm -rf /node-data/cfgsync
 set -e
 
 export CFG_SERVER_MODE="setup" \
-       CFG_SERVER_STORAGE_PATH="/node-data/cfgsync/deploymen-settings.yaml"
+       CFG_SERVER_STORAGE_PATH="/node-data/cfgsync/deploymen-settings.yaml" \
+       CHAIN_START_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+mkdir /node-data/cfgsync
 
 exec /usr/bin/logos-blockchain-cfgsync-server /etc/logos-blockchain/cfgsync.yaml
