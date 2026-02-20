557526f39a3fda6688417d532ad59c3a15188814
gusto <bacvinka@gmail.com>
2026-02-20 06:05:20 +0200

    Nginx serves node-data dir with configs and logs

--- testnet/scripts/run_node.sh
+++ testnet/scripts/run_node.sh
@@ -7,6 +7,7 @@ export CFG_FILE_PATH="/node-data/${LB_HOST_IDX}/config.yaml" \
        CFG_HOST_IDENTIFIER="i-${LB_HOST_IDX}" \
        CFG_DEPLOYMENT_PATH="/node-data/cfgsync/deployment-settings.yaml" \
        LOG_LEVEL="DEBUG" \
+       LOG_BACKEND="file" \
        LOG_DIR="/node-data/${LB_HOST_IDX}/"
 
 echo "Starting Faucet..."
