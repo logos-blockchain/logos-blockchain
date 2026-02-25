d4cc5c633ad08e91b48ff9c4a34156467325c301
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2026-02-25 21:28:25 +0900

    fix(test): create SDP config for non-blend nodes as well

--- tests/src/topology/configs/sdp.rs
+++ tests/src/topology/configs/sdp.rs
@@ -1,3 +1,5 @@
+use std::iter::repeat;
+
 use lb_core::{
     mantle::{GenesisTx as _, Op, genesis_tx::GenesisTx},
     sdp::DeclarationId,
@@ -9,16 +11,24 @@ pub struct GeneralSdpConfig {
 }
 
 #[must_use]
-pub fn create_sdp_configs(genesis_tx: &GenesisTx) -> Vec<GeneralSdpConfig> {
-    genesis_tx
-        .mantle_tx()
-        .ops
-        .iter()
+pub fn create_sdp_configs(genesis_tx: &GenesisTx, count: usize) -> Vec<GeneralSdpConfig> {
+    let ops = &genesis_tx.mantle_tx().ops;
+    assert!(
+        ops.len() <= count,
+        "genesis_tx contains {} declarations more than the requested number of configs: {count}",
+        ops.len()
+    );
+
+    ops.iter()
         .filter_map(|op| match op {
             Op::SDPDeclare(decl) => Some(GeneralSdpConfig {
                 declaration_id: Some(decl.id()),
             }),
             _ => None,
         })
+        .chain(repeat(GeneralSdpConfig {
+            declaration_id: None,
+        }))
+        .take(count)
         .collect()
 }
