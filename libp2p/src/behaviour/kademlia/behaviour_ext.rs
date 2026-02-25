de79935fba51b98f7425ce86fee17d0dbc08b9b8
andrussal <salumets.andrus@gmail.com>
2026-02-25 16:31:13 +0100

    fix: filter private ip + remove unreachable addr from kad

--- libp2p/src/behaviour/kademlia/behaviour_ext.rs
+++ libp2p/src/behaviour/kademlia/behaviour_ext.rs
@@ -23,6 +23,18 @@ impl<R: Clone + Send + RngCore + 'static> Behaviour<R> {
         }
     }
 
+    pub(crate) fn kademlia_remove_address(&mut self, peer_id: PeerId, addr: &Multiaddr) {
+        if self.kademlia.remove_address(&peer_id, addr).is_some() {
+            tracing::debug!("Removed address {:?} from peer {:?}", addr, peer_id);
+        } else {
+            tracing::debug!(
+                "Address {:?} for peer {:?} was not present in Kademlia",
+                addr,
+                peer_id
+            );
+        }
+    }
+
     pub(crate) fn kademlia_routing_table_dump(&mut self) -> HashMap<u32, Vec<PeerId>> {
         self.kademlia
             .kbuckets()
--- services/network/src/backends/libp2p/swarm/identify.rs
+++ services/network/src/backends/libp2p/swarm/identify.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-use lb_libp2p::libp2p::identify;
+use lb_libp2p::{Multiaddr, Protocol, libp2p::identify};
 use rand::RngCore;
 
 use crate::backends::libp2p::swarm::SwarmHandler;
@@ -30,6 +30,13 @@ impl<R: Clone + Send + RngCore + 'static> SwarmHandler<R> {
                     // we need to add the peer to the kademlia routing table
                     // in order to enable peer discovery
                     for addr in &info.listen_addrs {
+                        if !is_kademlia_candidate_address(addr) {
+                            tracing::debug!(
+                                "Skipping non-routable identify address for Kademlia: {}",
+                                addr
+                            );
+                            continue;
+                        }
                         self.swarm.kademlia_add_address(peer_id, addr);
                     }
                 }
@@ -38,5 +45,49 @@ impl<R: Clone + Send + RngCore + 'static> SwarmHandler<R> {
                 tracing::debug!("Identify event: {:?}", event);
             }
         }
+    }
+}
+
+fn is_kademlia_candidate_address(addr: &Multiaddr) -> bool {
+    for protocol in addr {
+        match protocol {
+            Protocol::Ip4(ip) => {
+                return !ip.is_loopback()
+                    && !ip.is_private()
+                    && !ip.is_unspecified()
+                    && !ip.is_link_local();
+            }
+            Protocol::Ip6(ip) => {
+                return !ip.is_loopback()
+                    && !ip.is_unspecified()
+                    && !ip.is_unique_local()
+                    && !ip.is_unicast_link_local();
+            }
+            _ => {}
+        }
+    }
+
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr as _;
+
+    use lb_libp2p::Multiaddr;
+
+    use super::is_kademlia_candidate_address;
+
+    #[test]
+    fn filters_non_routable_ipv4_addresses() {
+        let loopback = Multiaddr::from_str("/ip4/127.0.0.1/udp/1234/quic-v1").unwrap();
+        let private_192 = Multiaddr::from_str("/ip4/192.168.64.1/udp/1234/quic-v1").unwrap();
+        let private_10 = Multiaddr::from_str("/ip4/10.7.3.131/udp/1234/quic-v1").unwrap();
+        let public = Multiaddr::from_str("/ip4/8.8.8.8/udp/1234/quic-v1").unwrap();
+
+        assert!(!is_kademlia_candidate_address(&loopback));
+        assert!(!is_kademlia_candidate_address(&private_192));
+        assert!(!is_kademlia_candidate_address(&private_10));
+        assert!(is_kademlia_candidate_address(&public));
     }
 }
