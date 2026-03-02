d2f8f9c9eef7b359f8a2ee77283bed0e0a9f7c4c
Antonio Antonino <antonio@status.im>
2026-03-02 15:05:41 +0100

    Array from fn

--- core/src/proofs/leader_claim_proof.rs
+++ core/src/proofs/leader_claim_proof.rs
@@ -108,12 +108,9 @@ impl LeaderClaimPrivate {
             merkle_path_to_witness(voucher_path);
         let wallet = lb_poc::PoCWalletInputsData {
             secret_voucher: secret_voucher.into(),
-            voucher_merkle_path_and_selectors: voucher_merkle_path
-                .into_iter()
-                .zip(voucher_merkle_path_selectors)
-                .collect::<Vec<_>>()
-                .try_into()
-                .expect("Merkle path length should match the expected height"),
+            voucher_merkle_path_and_selectors: core::array::from_fn(|i| {
+                (voucher_merkle_path[i], voucher_merkle_path_selectors[i])
+            }),
         };
         let input = lb_poc::PoCWitnessInputsData::from_chain_and_wallet_data(chain, wallet);
         Self { input }
