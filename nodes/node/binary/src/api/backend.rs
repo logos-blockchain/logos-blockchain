81732748a1f58439db51a7596496d5c76a1ed714
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2026-03-29 17:57:28 +0900

    feat(api): add POST /channel/deposit

--- nodes/node/binary/src/api/backend.rs
+++ nodes/node/binary/src/api/backend.rs
@@ -45,7 +45,9 @@ use super::handlers::{
 use crate::{
     WalletService,
     api::{
-        handlers::{leader_claim, post_activity, post_declaration, post_withdrawal},
+        handlers::{
+            channel_deposit, leader_claim, post_activity, post_declaration, post_withdrawal,
+        },
         openapi::ApiDoc,
     },
 };
@@ -213,6 +215,12 @@ where
             .route(
                 paths::MEMPOOL_ADD_TX,
                 routing::post(add_tx::<MempoolStorageAdapter, RuntimeServiceId>),
+            )
+            .route(
+                paths::CHANNEL_DEPOSIT,
+                routing::post(
+                    channel_deposit::<WalletService, MempoolStorageAdapter, RuntimeServiceId>,
+                ),
             )
             .route(
                 paths::SDP_POST_DECLARATION,
