b0aaf89a89c7a473953fb8be86c642e2cecac93d
Antonio Antonino <antonio@status.im>
2026-04-01 15:41:57 +0200

    Last changes

--- blend/network/src/core/with_core/error.rs
+++ blend/network/src/core/with_core/error.rs
@@ -5,15 +5,18 @@ pub enum SendError {
     /// There were no peers to send a message to.
     NoPeers,
     /// The message being sent has an invalid public header.
-    InvalidMessage,
+    InvalidPublicHeader,
     /// The message being sent is a duplicate of a previous sent message.
     DuplicateMessage,
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub(crate) enum ReceiveError {
+    /// The received payload is not deserializable into an
+    /// `EncapsulatedMessage`.
+    UndeserializableMessage,
     /// The message being received has an invalid public header.
-    InvalidMessage,
+    InvalidPublicHeader,
     /// The message being received is a duplicate of a previous received
     /// message.
     DuplicateMessageFromPeer(PeerId),
