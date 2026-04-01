c8b3dd0453789dc794427ecf475647b1d29dc6a8
Antonio Antonino <antonio@status.im>
2026-04-01 12:58:26 +0200

    More changes

--- blend/network/src/core/with_core/behaviour/message_cache.rs
+++ blend/network/src/core/with_core/behaviour/message_cache.rs
@@ -8,6 +8,19 @@ use lb_blend_message::{
 };
 use libp2p::PeerId;
 
+/// Status of a message in the cache.
+///
+/// It can be either `Processed`, meaning that we have received and validated
+/// the message, but we haven't forwarded it to our peers yet, or `Forwarded`,
+/// meaning that we have already forwarded the message to our peers.
+///
+/// A message can move into the `Forwarded` state in one of two cases:
+/// - If we receive a message that we haven't seen before, we mark it as
+///   `Processed`, and then we forward it to our peers, marking it as
+///   `Forwarded` after forwarding it.
+/// - If we receive a message to forward from Blend service, we mark it as
+///   `Forwarded` immediately, since we won't forward it again nor process the
+///   same message if received from our peers.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum MessageStatus {
     /// Message has been received and validated, but not yet forwarded to
@@ -23,8 +36,7 @@ pub enum MessageStatus {
 /// same message multiple times.
 #[derive(Debug, Default)]
 pub struct MessageCache {
-    /// Map of message identifiers to whether we have only received or send and
-    /// received them.
+    /// Map of message identifiers to their status.
     messages: HashMap<MessageIdentifier, MessageStatus>,
     /// Map of peer identifiers to the set of message identifiers that we have
     /// seen from that peer, to be used when considering whether a peer is
@@ -33,8 +45,7 @@ pub struct MessageCache {
 }
 
 impl MessageCache {
-    /// Creates a new `MessageCache` with default capacity for the number of
-    /// peers that we expect to receive messages from.
+    /// Creates a new `MessageCache`.
     #[cfg(test)]
     pub fn new() -> Self {
         Self::default()
@@ -49,25 +60,33 @@ impl MessageCache {
         }
     }
 
-    /// Mark a message with the given identifier as received.
+    /// Mark a message as processed.
     ///
-    /// This function does not keep into account whether we already registered a
-    /// message as seen from a peer, but only whether the message was already
-    /// processed by us or not.
+    /// This means that we have received and validated the message, but we
+    /// haven't forwarded it to our peers yet.
+    ///
+    /// The function takes an `EncapsulatedMessageWithVerifiedPublicHeader` as
+    /// input, since we want to mark the message as processed only after
+    /// validating it.
     pub fn mark_message_as_processed(
         &mut self,
         message: &EncapsulatedMessageWithVerifiedPublicHeader,
     ) {
-        // Processed messages are also considered as received, so we only mark the
-        // message as received if it is not already marked as processed.
+        // Forwarded messages are also considered received (i.e. we ignore them if we
+        // receive them later on), so we only mark the message as received if it
+        // is not already marked as processed.
         let Entry::Vacant(entry) = self.messages.entry(message.id()) else {
             return;
         };
         entry.insert(MessageStatus::Processed);
     }
 
-    /// Mark a message with the given identifier as processed, meaning we won't
-    /// allow the swarm to send any duplicates of it.
+    /// Mark a message as forwarded, meaning we won't allow the swarm to send
+    /// any duplicates of it, nor process it if received from our peers.
+    ///
+    /// The function takes an `EncapsulatedMessageWithVerifiedPublicHeader` as
+    /// input, since we want to mark the message as forwarded only after
+    /// validating it.
     pub fn mark_message_as_forwarded(
         &mut self,
         message: &EncapsulatedMessageWithVerifiedPublicHeader,
@@ -75,15 +94,22 @@ impl MessageCache {
         self.messages.insert(message.id(), MessageStatus::Forwarded);
     }
 
-    /// Check whether a message with the given identifier has already been
-    /// received by us, meaning that we won't bubble it up to the swarm
-    /// again.
+    /// Check whether a message has already been processed by us, meaning that
+    /// we won't bubble it up to the swarm again. Forwarded messages are also
+    /// considered processed, so they will be included in the check.
+    ///
+    /// The function takes an `EncapsulatedMessage` as input, since we want to
+    /// check for duplicates before doing any expensive work validating the
+    /// message, since the message ID won't change before and after validation.
     pub fn is_message_processed(&self, message: &EncapsulatedMessage) -> bool {
         self.messages.contains_key(&message.id())
     }
 
-    /// Check whether a message with the given identifier has already been
-    /// processed (i.e. validated and forwarded) by us.
+    /// Check whether a message has already been forwarded by us.
+    ///
+    /// The function takes an `EncapsulatedMessage` as input, since we want to
+    /// check for duplicates before doing any expensive work validating the
+    /// message, since the message ID won't change before and after validation.
     pub fn is_message_forwarded(&self, message: &EncapsulatedMessage) -> bool {
         matches!(
             self.messages.get(&message.id()),
@@ -91,9 +117,12 @@ impl MessageCache {
         )
     }
 
-    /// Mark a message with the given identifier as seen from the given peer,
-    /// and return whether it was the first time we marked it as such for
-    /// that peer.
+    /// Mark a message as seen from the given peer, and return whether it was
+    /// the first time we marked it as such for that peer.
+    ///
+    /// The function takes an `EncapsulatedMessage` as input, since we want to
+    /// check for duplicates before doing any expensive work validating the
+    /// message, since the message ID won't change before and after validation.
     pub fn mark_message_as_seen_from_peer(
         &mut self,
         message: &EncapsulatedMessage,
@@ -120,6 +149,7 @@ impl MessageCache {
             .flat_map(|set| set.iter().copied())
     }
 
+    /// Get the status of a message in the cache, if it exists.
     #[cfg(test)]
     pub fn message_status(&self, message_id: &MessageIdentifier) -> Option<&MessageStatus> {
         self.messages.get(message_id)
