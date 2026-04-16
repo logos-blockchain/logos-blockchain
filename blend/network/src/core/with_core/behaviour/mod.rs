ae719e41206cf9e0694eba15120a5ee75b21de55
Antonio Antonino <antonio@status.im>
2026-04-16 08:09:22 +0200

    Add some more Blend trace logs

--- blend/network/src/core/with_core/behaviour/mod.rs
+++ blend/network/src/core/with_core/behaviour/mod.rs
@@ -978,6 +978,7 @@ where
             Either::Left(ConnectionHandler::new(
                 ConnectionMonitor::new(self.observation_window_clock_provider.interval_stream()),
                 self.protocol_name.clone(),
+                (peer_id, connection_id),
             ))
         } else {
             tracing::trace!(target: LOG_TARGET, "Denying inbound connection {connection_id:?} with edge peer {peer_id:?}.");
@@ -1026,6 +1027,7 @@ where
             Either::Left(ConnectionHandler::new(
                 ConnectionMonitor::new(self.observation_window_clock_provider.interval_stream()),
                 self.protocol_name.clone(),
+                (peer_id, connection_id),
             ))
         } else {
             tracing::debug!(target: LOG_TARGET, "Denying outbound connection {connection_id:?} with edge peer {peer_id:?}.");
