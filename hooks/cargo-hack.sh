fb18a3f2b1ef1ed6c393bd1a215099e322b07e95
Antonio Antonino <antonio@status.im>
2026-04-29 14:40:16 +0200

    Fix cargo-hack hook command

--- hooks/cargo-hack.sh
+++ hooks/cargo-hack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-RUSTFLAGS="-D warnings" cargo hack --feature-powerset --no-dev-deps --keep-going check
+RUSTFLAGS="-D warnings" cargo hack --feature-powerset --no-dev-deps --keep-going --all-targets check
