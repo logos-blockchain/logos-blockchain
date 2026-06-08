b38392982051ea22e6b7c656e51243ea54ef9d1c
gusto <bacvinka@gmail.com>
2026-06-08 20:02:07 +0300

    Update standalone config and provide ceremony files

---
+++ scripts/standalone-genesis-ceremony.sh
@@ -0,0 +1,7 @@
+./target/debug/logos-blockchain-tools-genesis ceremony \
+  --inscription-params testnet/ceremony/genesis/standalone/inscribe.yaml \
+  --stake-holders testnet/ceremony/genesis/standalone/stakeholders.yaml \
+  --providers testnet/ceremony/genesis/standalone/providers.yaml \
+  --faucet testnet/ceremony/genesis/standalone/faucet.yaml \
+  --deployment testnet/ceremony/genesis/standalone/deployment-template.yaml \
+  --output nodes/node/standalone-deployment-config.yaml
