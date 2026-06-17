76ba3910038a70c46f1eb44ec20e96e101050b0e
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2026-06-17 12:38:07 +0900

    chore(chain-leader): add warn log when proposal publishing is skipped
    Better to print an warning log than silently skipping publishing
    proposals when the `testing-disable-proposal-publish` feature is on.

--- services/chain/chain-leader/src/lib.rs
+++ services/chain/chain-leader/src/lib.rs
@@ -690,7 +690,11 @@ where
         #[cfg(not(feature = "testing-disable-proposal-publish"))]
         blend_adapter.publish_proposal(block.to_proposal()).await;
         #[cfg(feature = "testing-disable-proposal-publish")]
-        let _ = blend_adapter;
+        let _ = {
+            tracing::warn!(target: LOG_TARGET, "proposal publishing is disabled by the `testing-disable-proposal-publish` feature");
+            blend_adapter
+        };
+
         metrics::consensus_proposals_created_local();
     }
 
