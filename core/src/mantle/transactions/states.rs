d832628200e4f3134b97dcdd3e6fc49fc43c8f67
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-07-06 12:33:56 +0200

    Add transaction states.

---
+++ core/src/mantle/transactions/states.rs
@@ -0,0 +1,14 @@
+/// Marker trait for valid verification states of a transaction
+pub trait VerificationState {}
+
+/// Unverified state of a Transaction
+pub struct Unverified;
+impl VerificationState for Unverified {}
+
+/// Partially verified state of a Transaction
+pub struct Preverified;
+impl VerificationState for Preverified {}
+
+/// Fully verified state of a Transaction
+pub struct Verified;
+impl VerificationState for Verified {}
