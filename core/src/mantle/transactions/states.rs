f730e8ccec84d0154bc2cca7cab7fcbc4a50ccd9
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2026-08-19 21:40:10 +0900

    docs: modify comment for Verified

--- core/src/mantle/transactions/states.rs
+++ core/src/mantle/transactions/states.rs
@@ -6,7 +6,7 @@ pub trait VerificationState {}
 pub struct Unverified;
 impl VerificationState for Unverified {}
 
-/// Partially verified state of a Transaction/Operation
+/// Stateless-verified state of a Transaction/Operation
 ///
 /// This state indicates that the transaction has passed stateless checks. That
 /// is, checks that do not require access to the blockchain state, such as
@@ -15,7 +15,13 @@ impl VerificationState for Unverified {}
 pub struct Preverified;
 impl VerificationState for Preverified {}
 
-/// Fully verified state of a Transaction/Operation
+/// Stateful-verified state of a Transaction/Operation
+///
+/// This state indicates that the transaction has passed stateful checks, which
+/// require access to the blockchain state.
+/// ZK proof verifications are not included in this state, because they are deferred
+/// to the batch verification stage, which is performed after all transactions have
+/// been statefully verified.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Verified;
 impl VerificationState for Verified {}
