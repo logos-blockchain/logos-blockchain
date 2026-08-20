c0da8f7e679a221d7d29b02ec2773d57d9320d1b
Daniel <sanchez.quiros.daniel@gmail.com>
2026-08-20 16:49:06 +0200

    Add missing config files

---
+++ nodes/node/binary/src/config/pow/mod.rs
@@ -0,0 +1,23 @@
+use lb_pow_service::PoWServiceSettings;
+use lb_services_utils::overwatch::RecoveryData;
+
+use crate::config::pow::serde::Config;
+
+pub mod serde;
+
+pub struct ServiceConfig {
+    pub user: Config,
+}
+
+impl ServiceConfig {
+    #[must_use]
+    pub const fn into_pow_service_settings(
+        self,
+        recovery_data: RecoveryData,
+    ) -> PoWServiceSettings {
+        PoWServiceSettings {
+            claim_address: self.user.claim_address,
+            recovery_data,
+        }
+    }
+}
