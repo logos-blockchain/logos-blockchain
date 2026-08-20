52784f77cefa8a054e9a0b0d115ed140fa125a93
Daniel <sanchez.quiros.daniel@gmail.com>
2026-08-20 13:29:32 +0200

    Integrate service and config

--- services/pow/src/lib.rs
+++ services/pow/src/lib.rs
@@ -1,2 +1,8 @@
 mod service;
 mod tickets;
+
+pub use service::{
+    ClaimableRewardsInfo, PoWError, PoWService, PoWServiceMessage, PoWServiceSettings,
+    PoWServiceState,
+};
+pub use tickets::{TicketGenerator, WinningTicket};
