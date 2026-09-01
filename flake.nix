470abb1388ae0ea9b99cf7e2d80703a45c0fb40f
danielSanchezQ <3danimanimal@gmail.com>
2026-09-01 13:31:16 +0000

    Include hex files from blend fixtures in flake.nix build

--- flake.nix
+++ flake.nix
@@ -57,10 +57,18 @@
           pkgs = mkPkgs system;
           rustToolchain = pkgs.rust-bin.stable.${rustVersion}.default;
           craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+          # `craneLib.filterCargoSources` keeps only *.rs, *.toml, Cargo.lock and
+          # .cargo/config, so any non-Rust file pulled in with include_str!/
+          # include_bytes! has to be allowed through explicitly or the build
+          # fails with "couldn't read ...: No such file or directory".
+          assetSuffixes = [
+            ".hex"
+            "nodes/node/binary/src/config/deployment/settings.yaml"
+          ];
           src = pkgs.lib.cleanSourceWith {
             src = craneLib.path ./.;
             filter = path: type:
-              (pkgs.lib.hasSuffix "nodes/node/binary/src/config/deployment/settings.yaml" path) ||
+              (pkgs.lib.any (suffix: pkgs.lib.hasSuffix suffix path) assetSuffixes) ||
               (craneLib.filterCargoSources path type);
           };
           crateName = craneLib.crateNameFromCargoToml { inherit src; };
@@ -87,12 +95,12 @@
             RUSTFLAGS = "-L ${pkgs.libiconv}/lib";
           };
 
-          logosBlockchainDependencies = craneLib.buildDepsOnly (commonArgs);
+          logosBlockchainDependencies = craneLib.buildDepsOnly commonArgs;
 
           logosBlockChainC = craneLib.buildPackage (
             commonArgs
             // {
-              inherit logosBlockchainDependencies;
+              cargoArtifacts = logosBlockchainDependencies;
 
               postInstall = ''
                 mkdir -p $out/include
